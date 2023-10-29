5b40f392191b9e771769179a76cd8b15ec280b15
MahinMuhammad <amimahinurrahman@gmail.com>
2023-10-29 14:24:02 +0600

    Initial Commit

---
+++ GithubMonkey/GithubMonkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubMonkeyApp.swift
+//  GithubMonkey
+//
+//  Created by Md. Mahinur Rahman on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubMonkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
